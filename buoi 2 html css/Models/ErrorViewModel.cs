48e88c8d60e6f179d5153b32f4843a1917b68dbb
NN <NN@PC10>
2019-07-02 21:28:04 +0700

    hoa

---
+++ buoi 2 html css/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace buoi_2_html_css.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
